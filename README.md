c415a2f59e370e0bcbba2104ea81c83e395b08b9
Emanuel Frazão <emanuel.j.frazao@gmail.com>
2023-11-28 07:57:30 +0100

    initial app

---
+++ README.md
@@ -0,0 +1,5 @@
+# BMI app
+
+## Description
+
+a BMI calculator web-app
---
+++ myapp/main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from utils import calculate_bmi
+
+st.title("My first app")
+
+st.write("Enter your height in meters")
+height = st.number_input("Height")
+
+st.write("Enter your weight in kilograms")
+weight = st.number_input("Weight")
+
+if st.button("Calculate BMI"):
+    bmi = calculate_bmi(weight, height)
+    st.write("Your BMI is: ", bmi)
